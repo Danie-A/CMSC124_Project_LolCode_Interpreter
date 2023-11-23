3b218fb6ecf9a92f0ee996ecdcb0a80aae6be908
Danielle Araez <draraez@up.edu.ph>
2023-11-24 00:20:36 +0800

    chore: start ui; regex

--- lexical.py
+++ lexical.py
@@ -1,5 +1,4 @@
 # code for getting the lexemes of the .lol file
-
 from tkinter import *
 
 root = Tk()
@@ -9,16 +8,24 @@ topFrame = Frame(root)
 bottomFrame = Frame(root)
 
 fileExplorer = Label(topFrame, text="File Explorer")
-
-#defining text editor
 textEditor = Text(topFrame, width=43, height=10)
-textEditor.pack()
+tokensView = Label(topFrame, text="Token List")
+symbolTable = Label(topFrame, text="Symbol Table")
 
-tokenList = Label(topFrame, text="Token List")
+executeButton = Button(bottomFrame, text="Execute")
+console = Label(bottomFrame, text="Console")
+
+fileExplorer.grid(row=0, column=0, padx=10, pady=10)
+textEditor.grid(row=1, column=0, padx=10, pady=10)
+tokensView.grid(row=1, column=1, padx=10, pady=10)
+symbolTable.grid(row=1, column=2, padx=10, pady=10)
+
+executeButton.grid(row=0, column=0, padx=10, pady=10)
+console.grid(row=1, column=0, padx=10, pady=10)
 
 # display frames
-topFrame.pack()
-bottomFrame.pack(side=BOTTOM)
+topFrame.pack(side=TOP)
+bottomFrame.pack(side=TOP)
 
 # Set the width and height of the window
 window_width = 1200
@@ -33,3 +40,39 @@ y = (screen_height - window_height) // 3 # Add padding on top
 root.geometry(f"+{x}+{y}")
 
 root.mainloop()
+
+reg = {
+    "varident": r'^[a-zA-Z][a-zA-Z0-9_]*$',
+    "numbr_literal": r'^-?([1-9][0-9]*|0)$',
+    "numbar_literal": r'^(-?[0-9]*(\.[0-9]+)?)$',
+    "yarn_literal": r'^\"[^\"\']*\"$',
+    "troof_literal": r'^(WIN|FAIL)$',
+    "type_literal": r'^(NOOB|NUMBR|NUMBAR|YARN|TROOF)$',
+    "hai": r'^HAI$',
+    "kthxbye": r'^KTHXBYE$',
+    "wazzup": r'^WAZZUP$',
+    "buhbye": r'^BUHBYE$',
+    "btw": r'^BTW$',
+    "obtw": r'^OBTW$',
+    "tldr": r'^TLDR$',
+    "ihasa": r'^I HAS A$',
+    "itz": r'^ITZ$',
+    "r": r'^R$',
+    "sum": r'^SUM OF$',
+    "diff": r'^DIFF OF$',
+    "produkt": r'^PRODUKT OF$',
+    "quoshunt": r'^QUOSHUNT OF$',
+    "mod": r'^MOD OF$',
+    "biggr": r'^BIGGR OF$',
+    "smallr": r'^SMALLR OF$',
+    "both": r'^BOTH OF$',
+    "either": r'^EITHER OF$',
+    "won": r'^WON OF$',
+    "not": r'^NOT$',
+    "any": r'^ANY OF$',
+    "all": r'^ALL OF$',
+    "bothsaem": r'^BOTH SAEM$',
+    "diffrint": r'^DIFFRINT$',
+    "visible": r'^VISIBLE$',
+}
+
---
+++ try.lol
@@ -0,0 +1,35 @@
+HAI
+    WAZZUP
+        I HAS A var BTW uninitialized var
+        I HAS A var2 ITZ "some"
+        I HAS A var3 ITZ var2
+        I HAS A var4 ITZ SUM OF 5 AN 4
+        I HAS A num1 ITZ 8
+        I HAS A num2 ITZ 10
+        I HAS A ans
+    BUHBYE
+
+    var R BIGGR OF 3 AN 4
+    SMOOSH "one" AN "two" AN "four"
+    VISIBLE var IS NOW A TROOF
+
+    BOTH SAEM var4 AN 9
+    O RLY?
+        YA RLY
+            VISIBLE "TRUE"
+        NO WAI
+            VISIBLE "FALSE"
+    OIC
+
+    IM IN YR print10 UPPIN YR temp WILE DIFFRINT temp AN 10
+        VISIBLE temp
+    IM OUTTA YR print10
+
+    HOW IZ I sum_of_2_numbers YR num1 AN YR num2
+        ans R SUM OF num1 AN num2
+        FOUND YR ans
+    IF U SAY SO
+
+    VISIBLE I IZ sum_of_2_numbers YR num1 AN YR num2 MKAY
+
+KTHXBYE
